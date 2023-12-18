8cce5b92290bedbb864000c08c27a4bceaf67b77
Trần Trung Hiếu <tranhieu23.haui@gmail.com>
2023-12-18 10:47:14 +0700

    Complete

---
+++ PinterestBasicLayout/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  PinterestBasicLayout
+//
+//  Created by Tran Hieu on 03/12/2023.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
